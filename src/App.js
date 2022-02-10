5741ca3fb118d4728aa2226dbf76ea21ca2cbdb2
Galya Georgieva <galyarizova@gmail.com>
2022-02-10 10:57:35 +0200

    Added the beginnings of components and react router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
+    <>
+    <Routes>
+      <Route/>
+    </Routes>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +24,7 @@ function App() {
         </a>
       </header>
     </div>
+    </>
   );
 }
 
